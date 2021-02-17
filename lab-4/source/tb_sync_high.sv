71834fa1495b1250edfd108309c04cb726fb389d
David Chen <58645957+saymyname233@users.noreply.github.com>
2021-02-17 13:28:44 -0500

    Update tb_sync_high.sv

--- lab-4/source/tb_sync_high.sv
+++ lab-4/source/tb_sync_high.sv
@@ -1,8 +1,5 @@
 // $Id: $
 // File name:   tb_sync_high.sv
-// Created:     9/17/2020
-// Author:      Ti-Wei Chen
-// Lab Section: 337-09
 // Version:     1.0  Initial Design Entry
 // Description: Test bench for Reset to Logic Synchronizer
 
