f4e3a3d9d6463a7293f534b8f1cab9213b023bbe
David Chen <58645957+saymyname233@users.noreply.github.com>
2021-02-17 13:21:57 -0500

    Update adder_1bit.sv

--- lab-3/source/adder_1bit.sv
+++ lab-3/source/adder_1bit.sv
@@ -1,8 +1,5 @@
 // $Id: $
 // File name:   adder_1bit.sv
-// Created:     9/9/2020
-// Author:      Ti-Wei Chen
-// Lab Section: 337-09
 // Version:     1.0  Initial Design Entry
 // Description: 1-bit Full Adder Design
 
