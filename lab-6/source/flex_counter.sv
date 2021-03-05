094b621df15f47dc40eca2b9a96f94c9926e5bd5
David Chen <58645957+saymyname233@users.noreply.github.com>
2021-03-04 20:35:14 -0500

    Update flex_counter.sv

--- lab-6/source/flex_counter.sv
+++ lab-6/source/flex_counter.sv
@@ -1,8 +1,5 @@
 // $Id: $
 // File name:   flex_counter.sv
-// Created:     9/23/2020
-// Author:      Ti-Wei Chen
-// Lab Section: 337-09
 // Version:     1.0  Initial Design Entry
 // Description: Flexible Counter Design
 
