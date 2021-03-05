53b368e43b385e0be665194655a7717ea06f85b6
David Chen <58645957+saymyname233@users.noreply.github.com>
2021-03-04 20:52:32 -0500

    Update sr_9bit.sv

--- lab-6/source/sr_9bit.sv
+++ lab-6/source/sr_9bit.sv
@@ -1,8 +1,5 @@
 // $Id: $
 // File name:   sr_9bit.sv
-// Created:     10/7/2020
-// Author:      Ti-Wei Chen
-// Lab Section: 337-09
 // Version:     1.0  Initial Design Entry
 // Description: 9-bit Shift Register
 
